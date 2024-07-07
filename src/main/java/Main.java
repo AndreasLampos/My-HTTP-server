1912c21d11b6cb9810fca9d2d82224f54358f2aa
Andreas <anlampos@gmail.com>
2024-07-07 14:02:45 +0300

    responding with content try 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,19 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.io.*;
 
 public class Main {
   public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    // You can use print statements as follows for debugging, they'll be visible
+    // when running tests.
     System.out.println("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
-    
-     ServerSocket serverSocket = null;
-     Socket clientSocket = null;
-    
-     try {
+
+    ServerSocket serverSocket = null;
+    Socket clientSocket = null;
+
+    try {
       serverSocket = new ServerSocket(4221);
       // Since the tester restarts your program quite often, setting SO_REUSEADDR
       // ensures that we don't run into 'Address already in use' errors
@@ -28,20 +25,23 @@ public class Main {
       System.out.println(line);
       String[] httpPath = line.split(" ", 0);
       System.out.println(httpPath[1]);
+      String[] HttpRequest = line.split(" ", 0);
       OutputStream output = clientSocket.getOutputStream();
       if (httpPath[1].equals("/")) {
         output.write(("HTTP/1.1 200 OK\r\n\r\n").getBytes());
       } else if (httpPath[1].startsWith("/echo/")) {
-        output.write(("HTTP/1.1 200 OK\r\n\r\n").getBytes());
-        output.write(httpPath[1].getBytes());
-      } 
-      else {
+        String msg = HttpRequest[1].substring(6);
+        String header = String.format(
+            "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
+            msg.length(), msg);
+        clientSocket.getOutputStream().write(header.getBytes());
+      } else {
         output.write(("HTTP/1.1 404 Not Found\r\n\r\n").getBytes());
       }
-       System.out.println("accepted new connection");
-       clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-      } catch (IOException e) {
-       System.out.println("IOException: " + e.getMessage());
+      System.out.println("accepted new connection");
+      clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+    } catch (IOException e) {
+      System.out.println("IOException: " + e.getMessage());
     }
   }
 }
