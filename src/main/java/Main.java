6669b6e8afed18dd85b16eb4bcd527ab66a83880
Andreas <anlampos@gmail.com>
2024-07-07 15:40:50 +0300

    returning user agent

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -26,6 +26,7 @@ public class Main {
       String[] httpPath = line.split(" ", 0);
       System.out.println(httpPath[1]);
       String[] HttpRequest = line.split(" ", 0);
+      String[] str = HttpRequest[1].split("/");
       OutputStream output = clientSocket.getOutputStream();
       if (httpPath[1].equals("/")) {
         output.write(("HTTP/1.1 200 OK\r\n\r\n").getBytes());
@@ -35,6 +36,19 @@ public class Main {
             "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
             msg.length(), msg);
         clientSocket.getOutputStream().write(header.getBytes());
+        System.out.println("version");
+        String response = "HTTP/1.1 200 OK\r\n" + "Content-Type: text/plain\r\n" + "Content-Length: 0\r\n\r\n";
+        output.write(response.getBytes());
+      } else if (str[1].equals("user-agent")) {
+        reader.readLine();
+        String useragent = reader.readLine().split("\\s+")[1];
+        String reply = String.format(
+            "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %s\r\n\r\n%s\r\n", useragent.length(), useragent);
+        output.write(reply.getBytes());
+      } else if ((str.length > 2 && str[1].equals("echo"))) {
+        String responsebody = str[2];
+        String finalstr = "HTTP/1.1 200 OK\r\n" + "Content-Type: text/plain\r\n" + "Content-Length: " + responsebody.length() +"\r\n\r\n" + responsebody;
+        output.write(finalstr.getBytes());
       } else {
         output.write(("HTTP/1.1 404 Not Found\r\n\r\n").getBytes());
       }
