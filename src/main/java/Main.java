0836f954bc0d088ef249dc532bf726cacbe2fe83
Andreas <anlampos@gmail.com>
2024-07-07 13:50:31 +0300

    Path checking, if it's correct respond with 200, if not respond with 404.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,10 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.io.*;
 
 public class Main {
   public static void main(String[] args) {
@@ -13,11 +17,23 @@ public class Main {
      Socket clientSocket = null;
     
      try {
-       serverSocket = new ServerSocket(4221);
-       // Since the tester restarts your program quite often, setting SO_REUSEADDR
-       // ensures that we don't run into 'Address already in use' errors
-       serverSocket.setReuseAddress(true);
-       clientSocket = serverSocket.accept(); // Wait for connection from client.
+      serverSocket = new ServerSocket(4221);
+      // Since the tester restarts your program quite often, setting SO_REUSEADDR
+      // ensures that we don't run into 'Address already in use' errors
+      serverSocket.setReuseAddress(true);
+      clientSocket = serverSocket.accept(); // Wait for connection from client.`
+      InputStream inputStream = clientSocket.getInputStream();
+      BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+      String line = reader.readLine();
+      System.out.println(line);
+      String[] httpPath = line.split(" ", 0);
+      System.out.println(httpPath[1]);
+      OutputStream output = clientSocket.getOutputStream();
+      if (httpPath[1].equals("/")) {
+        output.write(("HTTP/1.1 200 OK\r\n\r\n").getBytes());
+      } else {
+        output.write(("HTTP/1.1 404 Not Found\r\n\r\n").getBytes());
+      }
        System.out.println("accepted new connection");
        clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
       } catch (IOException e) {
